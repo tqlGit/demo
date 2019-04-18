723401fa8c6f20c8a9eddfb774de97dc549029c4
tql <13683356327@126.com>
2019-04-18 17:26:44 +0800

    第一次提交

---
+++ javaTest/src/com/Test1ArrayList.java
@@ -0,0 +1,15 @@
+package com;
+
+import java.util.ArrayList;
+
+public class Test1ArrayList {
+	
+	
+	public static void main(String[] args) {
+		
+		ArrayList<String> list = new ArrayList<>(20);
+		System.out.println(list.size());
+		
+	}
+	
+}
